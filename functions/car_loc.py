0a3bb9245dff68d33c59a8ddb0dd413b276fa6f4
Filippo Musso <fili.musso05@gmail.com>
2024-10-28 13:24:41 +0100

    Accorpate funzioni di caricamento da file.

---
+++ functions/car_loc.py
@@ -0,0 +1,45 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import pandas as pd
+
+from utils.csv_reader import leggi_csv
+
+def carica_loc_arco(file_path):
+  localita = []
+  righe = leggi_csv(file_path)
+
+  for row in righe: 
+    cap = str(row[0]).strip()
+    loc = row[1].strip().strip('"')
+    prov = row[2].strip()
+    localita.append((cap, loc, prov))
+  
+  return localita
+
+def carica_dis_susa(file_path):
+  df = pd.read_excel(file_path, header=None)
+  
+  localita = []
+
+  for index, row in df.iterrows():
+    if row.iloc[4] in [3, 4]:
+      cap = str(row.iloc[0]).strip()
+      loc = row.iloc[1].strip()
+      prov = row.iloc[2].strip()
+      localita.append((cap, loc, prov))
+    
+  return localita
+
+def carica_alta_urb_susa(file_path):
+  localita = []
+  righe = leggi_csv(file_path)
+
+  for row in righe:
+    cap = str(row[1]).strip()
+    loc = row[0].strip().strip('"')
+    prov = row[2].strip()
+    localita.append((cap, loc, prov))
+  
+  return localita
